ac0fbc234d3367c0a59751745e4547698be0732a
faqexe <faqexe@icloud.com>
2023-01-28 19:34:38 +0300

    arr & string

--- Games/Cities/script.js
+++ Games/Cities/script.js
@@ -9,36 +9,49 @@ field.addEventListener("keydown", function (event) {
   if (event.keyCode === 13) {
     message.textContent = "";
     message.style.color = "";
-    cities.push(field.value.toLowerCase());
+    let firstChar = field.value.toLowerCase().charAt(0);
+    //  cities.push(field.value.toLowerCase());
+
+    if (cities.some((city) => city === field.value.toLowerCase())) {
+      message.textContent = "Такой город уже был";
+    } else {
+      cities.push(field.value.toLowerCase());
+      message.textContent = "Переход хода";
+    }
 
     // первая буква из поля ввода
-    let firstChar = field.value.toLowerCase().charAt(0);
-    console.log(firstChar);
+    //  let firstChar = field.value.toLowerCase().charAt(0);
+    //  console.log(firstChar);
     // Проверка совпадения букв
     if (cities.length > 1) {
       // ищем последнюю букву в последнем элементе массива
       let lastChar = cities[cities.length - 2].toString().slice(-1);
 
-      if (lastChar === firstChar) {
-        message.textContent = "Переход хода";
-        message.style.color = "green";
-      } else {
-        cities.pop();
+      if (lastChar !== firstChar) {
+        //   message.textContent = "Переход хода";
+        //   message.style.color = "green";
         message.textContent = "Не совпадает";
         message.style.color = "red";
       }
+      // else {
+      // //   cities.pop();
+      //   message.textContent = "Не совпадает";
+      //   message.style.color = "red";
+      // }
     }
 
-    console.log(cities);
+    //  console.log(cities);
 
     //** Рабочий код проверки */
     // проверка на такое же слово в массиве
-    //   if (cities.some((city) => city === field.value.toLowerCase())) {
-    //     message.textContent = "Такой город уже был";
-    //   } else {
-    //     cities.push(field.value.toLowerCase());
-    //     message.textContent = "Переход хода";
-    //   }
+    // if (cities.some((city) => city === field.value.toLowerCase())) {
+    //   message.textContent = "Такой город уже был";
+    // } else {
+    //   cities.push(field.value.toLowerCase());
+    //   message.textContent = "Переход хода";
+    // }
+
+    console.log(cities);
   }
 });
 
