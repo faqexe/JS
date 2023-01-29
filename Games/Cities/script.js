b47a54faefc2ba8d9e81c747fc7e8d5f8e0b03b3
faqexe <faqexe@icloud.com>
2023-01-29 13:47:29 +0300

    +

--- Games/Cities/script.js
+++ Games/Cities/script.js
@@ -1,46 +1,38 @@
 "use strict";
+//! Дорабоать функцию поиска в массиве
 
 let field = document.querySelector("#field");
 let message = document.querySelector("#message");
-
 let cities = [];
 
 field.addEventListener("keydown", function (event) {
   if (event.keyCode === 13) {
-    message.textContent = "";
-    message.style.color = "";
-    let firstChar = field.value.toLowerCase().charAt(0);
-    //  cities.push(field.value.toLowerCase());
-
-    if (cities.some((city) => city === field.value.toLowerCase())) {
-      message.textContent = "Такой город уже был";
-    } else {
-      cities.push(field.value.toLowerCase());
-      message.textContent = "Переход хода";
-    }
-
+    let word = field.value.toLowerCase();
     // первая буква из поля ввода
-    //  let firstChar = field.value.toLowerCase().charAt(0);
-    //  console.log(firstChar);
-    // Проверка совпадения букв
-    if (cities.length > 1) {
-      // ищем последнюю букву в последнем элементе массива
-      let lastChar = cities[cities.length - 2].toString().slice(-1);
+    let firstChar = word.charAt(0);
 
-      if (lastChar !== firstChar) {
-        //   message.textContent = "Переход хода";
-        //   message.style.color = "green";
+    let check = cities.some((word) => cities.includes(word, 1));
+
+    if (cities.length === 0) {
+      cities.push(word);
+      message.textContent = "Переход хода";
+      message.style.color = "green";
+      // Проверка совпадения букв
+    } else {
+      cities.unshift(word);
+      // ищем последнюю букву в предыдущем элементе массива
+      // let lastChar = cities[cities.length - 2].toString().slice(-1);
+      let lastChar = cities[1].toString().slice(-1);
+
+      if (lastChar === firstChar) {
+        message.textContent = "Переход хода";
+        message.style.color = "green";
+      } else {
         message.textContent = "Не совпадает";
         message.style.color = "red";
+        cities.shift();
       }
-      // else {
-      // //   cities.pop();
-      //   message.textContent = "Не совпадает";
-      //   message.style.color = "red";
-      // }
     }
-
-    //  console.log(cities);
 
     //** Рабочий код проверки */
     // проверка на такое же слово в массиве
@@ -54,5 +46,3 @@ field.addEventListener("keydown", function (event) {
     console.log(cities);
   }
 });
-
-//! Доделать проверку массива на совпадения
