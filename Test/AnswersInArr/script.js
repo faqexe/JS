b8f6be3fccd843dd03eb938ec586c344a7685f57
faqexe <faqexe@icloud.com>
2023-01-31 15:01:39 +0300

    test_arr

--- Test/AnswersInArr/script.js
+++ Test/AnswersInArr/script.js
@@ -1,7 +1,16 @@
-'use strict';
+"use strict";
 
-let answers = [
-	'ответ 1',
-	'ответ 2',
-	'ответ 3',
-];
+let inputs = document.querySelectorAll("input");
+let btn = document.querySelector("#button");
+
+let answers = ["ответ 1", "ответ 2", "ответ 3"];
+
+btn.addEventListener("click", function () {
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === answers[i]) {
+      inputs[i].classList.add("right");
+    } else {
+      inputs[i].classList.add("wrong");
+    }
+  }
+});
